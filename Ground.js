90d365d50761b32018f43dbac8c8efd8e4eee82d
PSDivyadarshini <74144949+PSDivyadarshini@users.noreply.github.com>
2021-02-06 20:58:31 +0530

    Add files via upload
    ball

---
+++ Ground.js
@@ -0,0 +1,29 @@
+class Ground{
+    
+        constructor(){
+        
+            var groundOptions={
+                isStatic:true
+            }
+            this.body=Bodies.rectangle(600,600,1200,20,groundOptions);
+            this.width=1200;
+            this.height=20;
+            World.add(world,this.body);
+        }
+        
+           display(){
+            var pos=this.body.position;
+            push();
+            rectMode(CENTER);
+            strokeWeight(4);
+            fill("black");
+            rect(pos.x,pos.y,this.width,this.height);
+            pop();
+           }
+        
+        
+        
+        
+            
+        }
+    
